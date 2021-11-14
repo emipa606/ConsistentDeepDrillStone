349c6a016d7656b6f9fc2f8ebc507053e7e2adf2
Evelyn <udderlyevelyn@gmail.com>
2021-11-14 13:55:20 -0500

    Add project files.

---
+++ 1.3/Source/ConsistentDeepDrillStone/ConsistentDeepDrillStone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using HarmonyLib;
+
+namespace CDDS
+{
+    [StaticConstructorOnStartup]
+    public static class ConsistentDeepDrillStone
+    {
+        static ConsistentDeepDrillStone()
+        {
+            Log.Message("Consistent Deep Drill Stone Loaded!");
+            var harmony = new Harmony("UdderlyEvelyn.ConsistentDeepDrillStone");
+            harmony.PatchAll();
+        }
+    }
+}
