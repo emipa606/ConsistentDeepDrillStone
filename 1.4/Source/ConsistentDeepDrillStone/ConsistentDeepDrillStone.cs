37ea43911d1dd8f4c872500fdd9db3ed9f3b249e
Insanity_Prelude <jiopaba@gmail.com>
2022-11-13 22:20:38 -0500

    1.4 Update
    Didn't bother switching over to a "Common" / Version Specific architecture.  The 1.3 folder was just copied to 1.4 with a new re-compiled Binary.  Been working in my game with extensive use and zero issues for about 12 hours now.

---
+++ 1.4/Source/ConsistentDeepDrillStone/ConsistentDeepDrillStone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using HarmonyLib;
+
+namespace CDDS
+{
+    [StaticConstructorOnStartup]
+    public static class ConsistentDeepDrillStone
+    {
+        static ConsistentDeepDrillStone()
+        {
+            var harmony = new Harmony("UdderlyEvelyn.ConsistentDeepDrillStone");
+            harmony.PatchAll();
+        }
+    }
+}
